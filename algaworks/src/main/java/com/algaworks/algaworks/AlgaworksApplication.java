4edef88862e98778e154820af20b6c17a7a69da7
hermogenes.silva <hermogenes.silva@sesc-am.com.br>
2022-08-11 22:32:21 -0400

    Initial commit

---
+++ algaworks/src/main/java/com/algaworks/algaworks/AlgaworksApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksApplication.class, args);
+	}
+
+}
---
+++ algaworks/src/test/java/com/algaworks/algaworks/AlgaworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

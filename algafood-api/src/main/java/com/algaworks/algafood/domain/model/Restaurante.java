8d2e3eb10931a88aa6f2fd13d47eb98b49cc8890
hermogenes.silva <hermogenes.silva@sesc-am.com.br>
2022-09-01 18:17:35 -0400

    Criando infraestrutura para cidade, permissao, estado e forma pagamento
    iniciando o processo de criação de infra cidade, permissao, estado e forma pagamento

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -22,7 +22,8 @@ public class Restaurante {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@Column(nullable = false)
 	private String nome;
 	
 	@Column(name = "taxa_frete", nullable = true)

21ce4a010f6d7bc88ecc26b314bdbcb03578f394
hvivox <hvivox@hotmail.com>
2022-09-06 22:27:16 -0400

    criacao do recurso estado e cozinha
    implementação dos recursos rest para consultar estado e cozinha

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafood.api.controller;
+
+
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Estados")
+public class EstadoController {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+
+    @GetMapping
+    public List<Estado> lista(){
+        return estadoRepository.listar();
+    }
+
+
+    @GetMapping("/{estadoId}")
+    public Estado buscar( @PathVariable Long idEstado ){
+        return estadoRepository.buscar(idEstado);
+    }
+
+}
